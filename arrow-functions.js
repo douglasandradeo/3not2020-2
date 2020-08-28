e93e17c4ddafbc68d367f08acb2d204c585b10ce
douglasandradeo <douglasandradeoliv10@gmail.com>
2020-08-28 23:56:53 +0000

    (28/8) Finalização revisão e início de atividade. Entrega próxima terça.

--- arrow-functions.js
+++ arrow-functions.js
@@ -1,4 +1,5 @@
 // Expressão de função "Tradicional"
+// testar = node nomedoarquivo.js
 
 // Características desta função
 // - Um único argumento -> n
@@ -38,6 +39,56 @@ console.log('--------------------------')
 let aleatorio2 = () => Math.floor(Math.random() * 1000)
 
 console.log(aleatorio2())
+
+// Situação alternativa 2: função com mais de 1+ argumentos
+
+// Função tradicional com 1+ argumentos
+let imc = function(peso, altura) {
+    return peso / (altura ** 2)
+}
+console.log(imc(82, 1.78))
+
+console.log('--------------------------')
+
+// Arrow function com 1+ argumento
+// Os parênteses dos argumentos são obrigatórios quando No argumentos > 1
+let imc2 = (peso, altura) => peso / (altura **2)
+console.log(imc(82, 1.78))
+console.log(imc2(82, 1.78))
+
+// CONCLUSÃO: Os parênteses só podem ser omitidos da lista de argumentos
+// de uma arrow function quando há UM e APENAS UM argumento.
+
+console.log('--------------------------')
+
+// Situação alternativa 3: funções com mais de uma linha no corpo
+
+// Função tradicional
+// 5! = 5 * 4 * 3 * 2 * 1 (120)
+let fatorial1 = function(n) {
+    let res = 1
+    for(let i = n; i > 1; i--) {
+        res *= i
+    }
+    return res
+}
+
+// Arrow function com corpo maior que uma linha
+//  - Não há como fazer otimizações quanto ao corpo da função
+let fatorial2 = n => {
+    let res = 1
+    for(let i = n; i > 1; i--) {
+        res *= i
+    }
+    return res
+}
+
+console.log(fatorial1(5))
+console.log(`O resultado é ${fatorial2(5)}.`)
+
+
+
+
 
 
 
