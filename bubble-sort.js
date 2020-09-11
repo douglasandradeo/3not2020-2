24c2d973302c97376b5d07e7c21cb01fbac62241
douglasandradeo <douglasandradeoliv10@gmail.com>
2020-09-11 23:48:06 +0000

    (11/9) Bubble Sort

---
+++ bubble-sort.js
@@ -0,0 +1,61 @@
+/*
+    Algoritmo e ordenação Bubble Sort
+
+    Estratégia:
+    Percorrer o vetor de dados, comparando elementos adjacentes
+    e promovendo a troca quando o sucessor é maior que o antecessor.
+
+    O percurso no vetor é feito tantas vezes quando necessário, até
+    que nenhuma troca seja efetuada no percurso.
+*/ 
+
+function bubbleSort(vetor) {
+    let passadas = 0, comparacoes = 0, totalTrocas = 0
+    let trocas
+    do {
+        passadas++
+        trocas = 0
+
+        // Percurso do vetor até a PENÚLTIMA POSIÇÃO (length -2)
+        for(let i = 0; i <= vetor.length - 2; i++) {
+            comparacoes++
+            // Comparando o elemento da posição atual (i)
+            // com o elemento da frente (i + 1)
+            if(vetor[i] > vetor [i + 1]) {
+                // Troca é feita de uma forma desestruturada,
+                // meio que existe dentro do JavaScript
+                [vetor[i], vetor [i + 1]] = [vetor[i + 1], vetor [i]]
+                // Faz contagem das trocas
+                trocas++
+                totalTrocas++
+            }
+        }
+    } while (trocas > 0)
+    // Exibindo as estatísticas
+    console.log('Passadas:', passadas,'Comparações:', comparacoes,'Total de trocas:', totalTrocas)
+}
+
+let nums =[58, 16, 33, 82, 4, 47, 25, 71, 96, 60, 41, 89]
+
+console.time('teste1')
+console.log(bubbleSort(nums))
+console.timeEnd('teste1')
+
+console.log(nums)
+
+/* console.log('-------------------')
+
+let nomeEmpresas = require('./dados/15-mil-empresas')
+
+console.time('Empresas')
+console.log(bubbleSort(nomeEmpresas))
+console.timeEnd('Empresas') */
+
+console.log('-------------------')
+
+let listanomes = require('./dados/100-mil-nomes')
+
+console.time('Nomes')
+console.log(bubbleSort(listanomes))
+console.timeEnd('Nomes')
+
