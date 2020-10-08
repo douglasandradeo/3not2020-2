a1a7aab7cd40c43e31624f221336bd4fb76e668d
douglasandradeo <douglasandradeoliv10@gmail.com>
2020-10-08 23:29:49 +0000

    (8/10) Início quickSort

--- quick-sort.js
+++ quick-sort.js
@@ -1,3 +1,4 @@
+/*
 function quickSort(vetor) { 
     
 
@@ -18,6 +19,47 @@ function quickSort(vetor) {
         }
     }
 }
+*/
 
+let passadas = 0, comparacoes = 0, trocas = 0
+function quickSort(vetor, inicio = 0, fim = vetor.length - 1) {
+    //console.log({vetor, inicio, fim})
+    if(fim > inicio) { // Garante que haja, PELO MENOS, DOIS elementos para ordenar
+        passadas++
+        let posDiv = inicio - 1
+        let posPivot = fim
 
-    
+        for(let i = inicio; i < fim; i++) {
+            comparacoes++
+            if(vetor[i] < vetor[posPivot]) { 
+                posDiv++
+                [vetor[i], vetor[posDiv]] = [vetor[posDiv], vetor[i]]
+                trocas++
+            }
+        }
+        // Último incremento de posDiv, após o loop terminar
+        posDiv++
+        [vetor[posDiv], vetor[posPivot]] = [vetor[posPivot], vetor[posDiv]]
+        trocas++
+        //console.log({posDiv})
+
+        quickSort(vetor, inicio, posDiv - 1) // lado esquerdo
+        quickSort(vetor, posDiv + 1, fim)    // lado direito
+    }
+}   
+
+/*
+let nums = [55, 5, 10, 15, 50, 45, 40, 35, 20, 25, 30]
+
+quickSort(nums)
+console.log(nums)
+console.log({passadas, comparacoes, trocas})
+*/
+
+const nomes = require('./dados/100-mil-nomes')
+console.time('teste')
+quickSort(nomes)
+console.timeEnd('teste')
+console.log(nomes)
+console.log('Memória usada (MB): ', process.memoryUsage().heapUsed / 1024 / 1024)
+console.log({passadas, comparacoes, trocas})
