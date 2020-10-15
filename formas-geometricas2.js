bc594a7e4fca85270f06402fa29886c73dca0351
douglasandradeo <douglasandradeoliv10@gmail.com>
2020-10-15 23:30:51 +0000

    (15/10) Classes

--- formas-geometricas2.js
+++ formas-geometricas2.js
@@ -1,15 +1,59 @@
+// class faz com que haja uma padrão na estrutura dos objetos da mesma classe
+
 class FormaGeometrica {
-    constructor(b, a, t) {
-        this.base = b
-        this.altura = a
-        this.tipo = t
+    constructor(base = 1, altura = 1, tipo = 'R') {
+
+        if(isNaN(base) || base <= 0) {
+            throw new Error(`Valor de base: ${base} inválido.`)
+        }
+
+        if(isNaN(altura) || altura <= 0) {
+            throw new Error('A altura deve ser númerica e maior que zero.')
+        }
+        
+        //if(tipo != 'R' && tipo != 'T' && tipo != 'E') {
+        if(['R', 'T', 'E'].indexOf(tipo) < 0) {
+            throw new Error('O tipo deve ser R, T ou E.')
+        }
+
+        this.base = base
+        this.altura = altura
+        this.tipo = tipo
+    }
+
+    // como "area" está dentro do objeto, não precisa receber mais informações sobre sua forma
+    area() {
+        switch(this.tipo) {
+            case 'R':
+                return this.base * this.altura
+            case 'T':
+                return this.base * this.altura / 2
+            case 'E':
+                return (this.base / 2) * (this.altura / 2) * Math.PI
+            default: //Tipo inválido
+                return undefined
+        }
     }
 }
 
-const retangulo = new FormaGeometrica(4, 6.5, 'R')
+const retangulo = new FormaGeometrica(2, 6.5, 'R')
 const triangulo = new FormaGeometrica(4, 6.5, 'T')
 const elipse = new FormaGeometrica(4, 6.5, 'E')
+const teste = new FormaGeometrica(12, 'arra', 'Y')
 
-console.log(retangulo.base)
-console.log(triangulo.base)
-console.log(elipse.base)
+console.log(retangulo)
+console.log(triangulo)
+console.log(elipse)
+console.log(teste)
+
+console.log(retangulo.area())
+
+
+// criamos o objeto com valores-padrão mas é possível alterá-los
+retangulo.altura = 6
+retangulo.base = 2.75
+console.log(retangulo.area())
+
+console.log('Área do teste', teste.area())
+
+console.log('Área do teste', teste.area())
